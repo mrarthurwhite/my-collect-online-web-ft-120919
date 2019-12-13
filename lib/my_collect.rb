f491888cb9e7e1f376e08afaf00fa01e974feeae
Arthur White <mister.arthur.white@gmail.com>
2019-12-13 04:49:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,7 +5,15 @@ def my_collect(a)
   # and execute the code in the block you call it 
   # with for each element in the collection (use the yield keyword). 
   # It should return the modified collection.
+  i = 0
+  while i < a.length
+    yield (a [i])
+    i +=1
+  end
   
 end
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
