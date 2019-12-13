75da08e78a5827ba32b71e8665d3d487222c480f
Arthur White <mister.arthur.white@gmail.com>
2019-12-13 04:47:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,10 @@
 def my_collect
   
-  
+  # It should take in an argument of a collection, 
+  # iterate over that collection using a while loop, 
+  # and execute the code in the block you call it 
+  # with for each element in the collection (use the yield keyword). 
+  # It should return the modified collection.
   
 end
 
